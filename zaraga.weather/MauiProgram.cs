aae61a3018a6f1c3cbfd8111ecfd957740b56919
Angel Gustavo Garza Aguilar <angel_winy@live.com.mx>
2025-05-19 23:46:09 -0600

    Add ProgressView Package

--- zaraga.weather/MauiProgram.cs
+++ zaraga.weather/MauiProgram.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using Microsoft.Maui.Controls.Hosting;
 using Microsoft.Maui.Hosting;
+using SkiaSharp.Views.Maui.Controls.Hosting;
 
 namespace zaraga.weather;
 
@@ -11,6 +12,7 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            .UseSkiaSharp()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
--- zaraga.weather/Pages/Home/HomePage.xaml.cs
+++ zaraga.weather/Pages/Home/HomePage.xaml.cs
@@ -1,6 +1,8 @@
+using Microsoft.Maui.ApplicationModel;
 using Microsoft.Maui.Controls;
 using System;
 using System.Diagnostics;
+using System.Threading.Tasks;
 using zaraga.weather.Models;
 using zaraga.weather.Services;
 
@@ -13,8 +15,24 @@ public partial class HomePage : ContentPage
         InitializeComponent();
     }
 
-    protected override void OnAppearing()
+    protected override async void OnAppearing()
     {
         base.OnAppearing();
+
+        try
+        {
+            IsBusy = true;
+
+
+            await Task.Delay(3000);
+            var resp = await ApiService.Instance.GetRequest<ErrorResponse>("current.json?key=7d848ee5e8d34b84bf1211825250205&q=19.387313970312963, -99.06179532153836&aqi=no", new ErrorResponse());
+            Debug.WriteLine(resp.message);
+            IsBusy = false;
+        }
+        catch (Exception ex)
+        {
+            IsBusy = false;
+            await Shell.Current.DisplayAlert("Error", ex.Message, "ok");
+        }
     }
 }
