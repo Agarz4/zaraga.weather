086c81c076d92b9e3783854c84f5d094bda9882b
Angel Garza <agarza@aph.mx>
2025-05-26 20:13:26 -0600

    control template global para mostrar la linea de carga usando BindableProperties

--- zaraga.weather/Services/SharedViewModel.cs
+++ zaraga.weather/Services/SharedViewModel.cs
@@ -4,51 +4,55 @@ using Microsoft.Maui.ApplicationModel;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Devices.Sensors;
 using zaraga.weather.Services;
+using System;
 
-namespace zaraga.weather
+namespace zaraga.weather;
+
+internal class SharedViewModel : PropertyChangedViewModel
 {
-    internal class SharedViewModel : PropertyChangedViewModel
+    private bool _isLoading = false;
+
+    public bool IsLoading { get => _isLoading; set { _isLoading = value; OnPropertyChanged(); } }
+
+
+    private readonly DeviceSensors _devicefeatures;
+
+
+    internal SharedViewModel()
     {
-        private readonly DeviceSensors _devicefeatures;
+        _devicefeatures = new DeviceSensors();
+    }
 
-        internal SharedViewModel()
+    /// <summary>
+    /// Obtiene todos los permisos necesarios
+    /// </summary>
+    internal async Task GetAppPermissions()
+    {
+        App.Log("Getting location in use permission");
+        var locationPermission = await _devicefeatures.CheckAndRequestPermissionAsync(new Permissions.LocationWhenInUse());
+        App.Log($"Location permission status: {locationPermission}");
+    }
+
+    /// <summary>
+    /// Obtiene la ubicacion actual del dispositivo si se cuenta con el permiso.
+    /// </summary>
+    internal async Task<Location?> GetDeviceLocation()
+    {
+        App.Log("Checking location in use permission");
+        var locationPermission = await _devicefeatures.CheckPermissionStatus(new Permissions.LocationWhenInUse());
+        App.Log($"Location permission status: {locationPermission}");
+        if (locationPermission == PermissionStatus.Granted)
         {
-            _devicefeatures = new DeviceSensors();
+            App.Log("Getting Current location");
+            var deviceLocation = await _devicefeatures.GetCurrentLocation();
+            App.Log($"Current location: Latitude:{deviceLocation?.Latitude}, Longitude:{deviceLocation?.Longitude}");
+            return deviceLocation;
         }
-
-        /// <summary>
-        /// Obtiene todos los permisos necesarios
-        /// </summary>
-        /// <returns></returns>
-        internal async Task GetAppPermissions()
+        else
         {
-            App.Log("Getting location in use permission");
-            var locationPermission = await _devicefeatures.CheckAndRequestPermissionAsync(new Permissions.LocationWhenInUse());
-            App.Log($"Location permission status: {locationPermission}");
-        }
-
-        /// <summary>
-        /// Obtiene la ubicacion actual del dispositivo si se cuenta con el permiso.
-        /// </summary>
-        /// <returns></returns>
-        internal async Task<Location?> GetDeviceLocation()
-        {
-            App.Log("Checking location in use permission");
-            var locationPermission = await _devicefeatures.CheckPermissionStatus(new Permissions.LocationWhenInUse());
-            App.Log($"Location permission status: {locationPermission}");
-            if (locationPermission == PermissionStatus.Granted)
-            {
-                App.Log("Getting Current location");
-                var deviceLocation = await _devicefeatures.GetCurrentLocation();
-                App.Log($"Current location: Latitude:{deviceLocation?.Latitude}, Longitude:{deviceLocation?.Longitude}");
-                return deviceLocation;
-            }
-            else
-            {
-                // Permission denied, handle accordingly
-                await Shell.Current.DisplayAlert("Permiso denegado", "No se cuenta con el permiso para acceder a la ubicación del dispositivo", "OK");
-                return null;
-            }
+            // Permission denied, handle accordingly
+            await Shell.Current.DisplayAlert("Permiso denegado", "No se cuenta con el permiso para acceder a la ubicación del dispositivo", "OK");
+            return null;
         }
     }
 }
